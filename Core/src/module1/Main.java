3a4d3dbb7d2a51fdca82bce5594b7f65da5f5b92
Xuan Manh 9999 <nguyenxuanmanh030929@gmail.com>
2025-10-09 23:08:37 +0700

    Add

---
+++ Core/src/module1/Main.java
@@ -0,0 +1,20 @@
+package module1;
+
+public class Main {
+    public static void main(String[] args) {
+        // jvm => là máy ảo của java
+        // int a = 1; printf("%d", a) -> C
+        // c => mã máy(0, 1) => ra kết qua => 1 (ngôn ngữ lập trình bậc thấp)
+        // Ngôn ngữ lập trình bâc thấp => nó gần với mã máy và khó hiểu cho con người
+        // java => jvm (.class) => mã máy => kết quả (ngôn ngữ lập trình bậc cao)
+        // Nó gần với cách hiểu của người
+
+//        Thông dịch
+//        Java là một ngôn ngữ lập trình vừa biên dịch vừa thông dịch. Chương trình nguồn viết bằng ngôn ngữ
+//        lập trình Java có đuôi _.java đầu tiên được biên dịch thành tập tin có đuôi _.class và sau đó sẽ
+//                được trình thông dịch thông dịch thành mã máy.
+        // Biên dịch => chuyển đổi từ dạng này qua dạng khác (chung cấu trúc)
+        // Thông dịch (chỉ thông dịch nếu có máy ảo) => chuyển đổi qua mã máy để mã máy hiểu
+        System.out.println("Manh");
+    }
+}
---
+++ Core/src/module2/Main.java
@@ -0,0 +1,79 @@
+package module2;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("\uD83E\uDD23");
+//        System.out.println("❤️");
+        // bảng mã Unicode
+//      Java được xây dựng trên bảng chữ cái Unicode, một hệ thống mã hóa quốc tế bao gồm hầu hết các ngôn ngữ và biểu
+//      tượng trên thế giới. Điều này cho phép ngôn ngữ Java hỗ trợ nhiều ngôn ngữ và ký tự đặc biệt.
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("i: " + i);
+//            for (int j = 0; j <= 10; j++) {
+//                if (i >=  1) {
+//                    break;
+//                }
+//                System.out.println("j: " + j );
+//            }
+//        }
+//
+//        int a = 1;// định danh a
+//        int b = 2;// định danh b
+
+        // Quy tắc định danh trong java
+//        Trong java tên được đặt theo quy tắc sau:
+    //        Không trùng với từ khoá VD: int int  = 1; tên biến int trùng từ khoá
+    //        Không bắt đầu bằng một số, tên phải bắt đầu bằng kí tự hoặc bắt đầu bằng kí tự $,_
+    //        Không chứa dấu cách, các kí tự toán học như +, -, *,/, %..
+    //        Không trùng với một định danh khác trong cùng một phạm vi
+
+//        int 123 = 1;
+//        int 1a = 2;
+//        int $_1 = 1;
+//        int _1 = 1;
+//        int a*b  = 1;
+
+//        String b = "a";
+//        int b = 2;
+
+        // khuyên dùng khi định danh trong java
+//        Tên nên đặt sao cho có thể mô tả được đối tượng trong thực tế
+//        Java có phân biệt chữ hoa chữ thường
+//        Trong java ta có thể đặt tên với độ dài tuỳ ý
+//        Ta có thể sử dụng các kí tự tiếng việt để đặt tên
+//        int mạnh = 12;
+//        System.out.println(mạnh);
+
+//        String product = "Bim bim";
+//        String Product = "Kẹo mút";
+
+//        Đối với biến và phương thức thì tên bao giờ cũng bắt đầu bằng ký tự thường, nếu tên có nhiều từ thì ghép lại thì: ghép tất cả các từ thành một, ghi từ đầu tiên chữ thường,
+//        viết hoa kí tự đầu tiên của mỗi từ theo sau trong tên, ví dụ area, radius, readInteger, …
+//        Đối với tên lớp, giao diện (interface) ta viết hoa các kí tự đầu tiên của mỗi từ trong tên, ví dụ lớp WhileTest, Circle
+//        Tên hằng bao giờ cũng viết hoa, nếu tên gồm nhiều từ thì chúng được nối với hau bởi kí tự gạch dưới ‘_’, ví dụ PI, MAX_VALUE;
+
+//        int a = 1;
+//        final float PI = 3.13F;
+            // Khối lệnh trong java {} được sử dụng để tạo phạm vi cục bộ (local scope). Phạm vi cục bộ nghĩa là các biến hãy
+        // các giá trị khác nếu được định nghĩa trong khối lệnh thì sẽ chỉ được sử dụng bên trong khối lệnh đó
+
+
+    }
+
+    public void sleep () {
+        // khối lệnh được
+        int a = 1;
+    }
+
+
+//    public class Person {
+//
+//    }
+//    interface User
+
+
+    // Đối và lớp là gì
+    // Lớp -> Lớp là một kiểu dữ liệu trong java, nó là một thành phần nguyên mẫu, chứa các thuộc tính, và phương thức. Nó
+    // dùng để tạo ra các đối tượng (instance)
+    // Đối tượng(instance) đối tượng là thể thể hiện của lớp, hay ta hiểu là đối tượng là thành phần được tạo ra từ mẫu lớp
+}
